20b28318e4a88a97b560b223f92b2cae6c4ed8e6
IamGatheru <davidgatheru490@gmail.com>
2024-09-16 22:31:58 +0300

    Modified my __init__.py

--- __init__.py
+++ __init__.py
@@ -39,4 +39,5 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
-    return app
+    if __name__ == '__main__':
+        app.run(debug=True)
